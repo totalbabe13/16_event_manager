88db297256a07ea115a5d66895b12c81e1b16a44
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-10-22 15:22:20 -0500

    convert headers intable into symbols using csv library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,23 +1,9 @@
-puts "EventManager Initialized!"
 
-#CHECKING IF FILE EXISTS?
-# - - - - - - - - - - - - - - -
-# if File.exist? "event_attendees.csv"
-# 	puts 'this file exists'
-# end	
+require "csv"
+puts "EventManager initialized."
 
-#READING FILE
-# - - - - - - - - - - - - - - -
-# contents = File.read "event_attendees.csv"
-# puts contents
-
-#READ FILE LINE BY LINE
-# - - - - - - - - - - - - - - -
-lines = File.readlines "event_attendees.csv"
-
-lines.each_with_index do |line,index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
   puts name
 end
