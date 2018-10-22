81fd9be2c3be87cb7019a912a7fb3f64243cd1e8
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-10-22 15:13:06 -0500

    notes on reading files BASIC

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,23 @@
 puts "EventManager Initialized!"
+
+#CHECKING IF FILE EXISTS?
+# - - - - - - - - - - - - - - -
+# if File.exist? "event_attendees.csv"
+# 	puts 'this file exists'
+# end	
+
+#READING FILE
+# - - - - - - - - - - - - - - -
+# contents = File.read "event_attendees.csv"
+# puts contents
+
+#READ FILE LINE BY LINE
+# - - - - - - - - - - - - - - -
+lines = File.readlines "event_attendees.csv"
+
+lines.each_with_index do |line,index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
