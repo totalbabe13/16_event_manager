81767d698a3f228de0895076e218264b79acc084
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-10-22 15:40:00 -0500

    creating method behavior to clean up data interaction

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,19 +1,26 @@
 
-require "csv"
+require 'csv'
+
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    "00000"
+  elsif zipcode.length < 5
+    zipcode.rjust(5,"0")
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
 puts "EventManager initialized."
 
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
 
-  if zipcode.nil?
-    zipcode = "00000"
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust 5, "0"
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  end
+  zipcode = clean_zipcode(row[:zipcode])
 
   puts "#{name} #{zipcode}"
 end
