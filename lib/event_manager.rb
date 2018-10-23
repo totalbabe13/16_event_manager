5f65c67a45439ad8f113b645ee61677181f2bb28
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-10-23 16:11:49 -0500

    refactoring behavior, created  method for interacting with data object from API

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,26 @@
-
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+                                  address: zip,
+                                  levels: 'country',
+                                  roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislators_string = legislator_names.join(", ")
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
 end
 
 puts "EventManager initialized."
@@ -16,21 +30,9 @@ contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :sy
 contents.each do |row|
   name = row[:first_name]
 
-  begin
-    zipcode = clean_zipcode(row[:zipcode])
+  zipcode = clean_zipcode(row[:zipcode])
 
-    legislators = civic_info.representative_info_by_address(
-                                address: zipcode,
-                                levels: 'country',
-                                roles: ['legislatorUpperBody', 'legislatorLowerBody'])
-    legislators = legislators.officials
+  legislators = legislators_by_zipcode(zipcode)
 
-    legislator_names = legislators.map(&:name)
-
-    legislators_string = legislator_names.join(", ")
-  rescue
-  	 "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
-  end
-  # puts civic_info
-  puts "#{name} #{zipcode} #{legislators_string}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
