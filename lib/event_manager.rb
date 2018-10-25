a3d9fe2dbefeeda7d9f1eb88d2ce1777d8295839
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-10-25 13:41:53 -0500

    finished date time and phone number clean up

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,56 +1,35 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
-
-# Iteration: Clean Phone Numbers
-# Similar to the zip codes the phone numbers suffer from multiple formats and inconsistencies. 
-# If we wanted to allow individuals to sign up for mobile alerts with the phone numbers we 
-# would need to make sure all of the numbers are valid and well-formed.
-
-# If the phone number is less than 10 digits assume that it is a bad number
-# If the phone number is 10 digits assume that it is good
-# If the phone number is 11 digits and the first number is 1, trim the 1 and use the first 10 digits
-# If the phone number is 11 digits and the first number is not 1, then it is a bad number
-# If the phone number is more than 11 digits assume that it is a bad number
+def get_hour(registration_time)
+  time_string = registration_time.gsub("/",'-')
+  reg_date =  DateTime.strptime(time_string.to_s, '%m-%d-%Y %H:%M')
+  reg_date.hour
+end	
 
 def clean_phone_numbers(phone_number)
-	#input is a random assormentment of STRING numbers
-
-	# step 1: convert phone number into a string with only digits
+	
 	phone_number_array = phone_number.split('')
 	only_digits = phone_number_array.keep_if { |digit| digit =~ /\d/ }
     num_count = only_digits.length
-
-    
-
 	if num_count == 10
 		only_digits.insert(3,'-')
         only_digits.insert(7,'-')
-		p only_digits.join
+	    only_digits.join
 	elsif num_count < 10
-		p 'invalid number'
+	    'invalid number'
 	elsif (num_count == 11)&&(only_digits[0] == 1)
 		only_digits.insert(3,'-')
         only_digits.insert(7,'-')
-		p only_digits.join
+	    only_digits.join
 	elsif num_count > 11
-		p 'invalid number'
+	    'invalid number'
 	elsif (num_count == 11)&&(only_digits[0] != 1)
-		p 'invalid number'
+	    'invalid number'
 	end	
-				
-				
-				
-				
-
-
-
-
-	#output : 123-456-7890
-
- 
-       					
+				       					
 end
 
 
@@ -89,10 +68,15 @@ contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :sy
 
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
+hours = []
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+# - - - - - - - 
+  hours << get_hour(row[:regdate])
+# - - - - - - -   
+
   zipcode = clean_zipcode(row[:zipcode])
 
   clean_phone_numbers(row[:homephone])
@@ -103,4 +87,7 @@ contents.each do |row|
 
   save_thank_you_letters(id,form_letter)
 end
+sum_of_hours = 0
+hours.each {|hour| sum_of_hours += hour }
+p sum_of_hours/19
 
